6202abbe63e9f3343f98be8ecb6d518d3c9bf62f
romano <luca.romano@student.reutlingen-university.de>
2023-02-17 20:08:06 +0100

    SOAP durch gRPC ausgetauscht

--- myaktion-monitor-quarkus/src/main/java/de/dpunkt/myaktion/monitor/ws/DonationDelegator.java
+++ myaktion-monitor-quarkus/src/main/java/de/dpunkt/myaktion/monitor/ws/DonationDelegator.java
@@ -2,38 +2,45 @@ package de.dpunkt.myaktion.monitor.ws;
 
 import de.dpunkt.myaktion.model.Donation;
 import de.dpunkt.myaktion.monitor.MonitorWebSocket;
+import io.quarkus.grpc.GrpcService;
+import io.smallrye.mutiny.Uni;
+import de.dpunkt.myaktion.MyaktionMonitorService;
+import de.dpunkt.myaktion.Myaktionmonitor;
+import de.dpunkt.myaktion.Myaktionmonitor.DonationRequest;
+import de.dpunkt.myaktion.Myaktionmonitor.DonationResponse;
 
+import javax.inject.Singleton;
 import javax.websocket.EncodeException;
 import javax.websocket.Session;
 import javax.websocket.server.PathParam;
-import javax.ws.rs.POST;
-import javax.ws.rs.Path;
 
 import java.io.IOException;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
-@Path("/sendDonationsToClients")
-public class DonationDelegator {
+@GrpcService
+public class DonationDelegator implements MyaktionMonitorService{
 
     private Logger logger = Logger.getLogger(DonationDelegator.class.getName());
 
-    //Diese Methode könnte potenziell über gRPC aufgerufen werden
-    @Path("/{campaignid}")
-    @POST
-    public void sendDonation(@PathParam("campaignid") Long campaignId) {
+    @Override
+    public Uni<DonationResponse> sendDonation(DonationRequest request) {
+        Long campaignId = request.getCampaignId();
+        de.dpunkt.myaktion.Myaktionmonitor.Donation donation = request.getDonation();
+
         for (Session session : MonitorWebSocket.getSessions()) {
             Long clientCampaignId = (Long) session.getUserProperties().get(
                     MonitorWebSocket.CAMPAIGN_ID);
             if (campaignId.equals(clientCampaignId)) {
                 try {
-                    session.getBasicRemote().sendObject(new Donation());
+                    session.getBasicRemote().sendObject(donation);
                 } catch (IOException | EncodeException e) {
                     logger.log(Level.INFO, "Keine Verbindung zu Client: "
                             + session, e);
                 }
             }
         }
+        return null;
     }
 
 }
