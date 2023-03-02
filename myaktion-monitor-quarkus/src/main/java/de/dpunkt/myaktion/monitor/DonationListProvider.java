a210f3a2a2f65b3aebd879085d827f311cdc23c5
romano <luca.romano@student.reutlingen-university.de>
2023-03-02 19:49:45 +0100

    ENV-Variablen für containerization gesetzt

--- myaktion-monitor-quarkus/src/main/java/de/dpunkt/myaktion/monitor/DonationListProvider.java
+++ myaktion-monitor-quarkus/src/main/java/de/dpunkt/myaktion/monitor/DonationListProvider.java
@@ -12,9 +12,10 @@ import org.eclipse.microprofile.rest.client.inject.RegisterRestClient;
 
 import de.dpunkt.myaktion.model.Donation;
 
-@RegisterRestClient(baseUri = "http://localhost:8443/donation/organizer/donation/list")
+@RegisterRestClient
 public interface DonationListProvider {
 
+    
     @GET
     @Path("/{campaignId}")
     @Produces(MediaType.APPLICATION_JSON)
